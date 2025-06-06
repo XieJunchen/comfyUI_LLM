680773535e80daf7b012e3cd0bddaa0c9b71f52f
xie junchen <1132348563@qq.com>
2025-06-06 23:55:08 +0800

    修复文件格式不对的问题

--- node/img_utils.py
+++ node/img_utils.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageOps
 from PIL.PngImagePlugin import PngInfo
 import requests
+import torch
 import numpy as np
 
 class LoadImageFromUrl:
@@ -13,7 +14,7 @@ class LoadImageFromUrl:
                 "url": (
                     "STRING",
                     {
-                        "default": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Example.jpg/800px-Example.jpg"
+                        "default": "http://swqqsa5wv.hb-bkt.clouddn.com/admin/comfyui_b46dcb7133234c549218bf957a6334ed.png"
                     },
                 ),
             }
@@ -24,18 +25,17 @@ class LoadImageFromUrl:
     FUNCTION = "load"
     CATEGORY = "云服务"
     
-    
     def load(self, url):
-        # 下载图片
         response = requests.get(url, stream=True, timeout=10)
         response.raise_for_status()
         image = Image.open(response.raw)
         image = ImageOps.exif_transpose(image)
-        # 转为 numpy 格式，float32，范围0~1，shape为(H, W, C)
         np_image = np.array(image).astype(np.float32) / 255.0
         if np_image.ndim == 2:  # 灰度图
             np_image = np.expand_dims(np_image, axis=-1)
-        return (np_image,)
+        # 保证输出为 (1, H, W, C)
+        tensor = torch.from_numpy(np_image).unsqueeze(0)
+        return (tensor,)
 
 NODE_CLASS_MAPPINGS = {
     "LoadImageFromUrl": LoadImageFromUrl
